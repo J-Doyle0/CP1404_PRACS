1bf4acc93f1a3510d1774a6508f6e896b7ff4c52
Jack <jackson.doyle@my.jcu.edu.au>
2022-09-25 19:02:27 +1000

    Copied from prac 1 and refactor program to use functions and added random scores

---
+++ Prac_02/scores.py
@@ -0,0 +1,24 @@
+"""CP1404 - Practical 1 - SCORES"""
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(f"A score of {score} is {determine_result(score)}.")
+    score = random.randint(0, 100)
+    print(f"A random score of {score} is {determine_result(score)}.")
+
+
+def determine_result(score):
+    """determines result from score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
