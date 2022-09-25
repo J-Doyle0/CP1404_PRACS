508fc1438d6ca52a93af0be85939902ad255631b
Jack <jackson.doyle@my.jcu.edu.au>
2022-09-25 18:30:28 +1000

    Refactor program to use functions

--- Prac_02/password_stars.py
+++ Prac_02/password_stars.py
@@ -1,11 +1,27 @@
-
-
+"""CP1404 - PRACTICAL 2 - PASSWORD STARS"""
 
 
 MINIMUM_LENGTH = 8
-password = input("Password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password doesn't meet requirements (minimum {MINIMUM_LENGTH} letters)")
-    password = input("Password: ")
-for char in range(0, len(password)):
-    print("*", end="")
+
+
+def main():
+    password = get_password()
+    display_asterisks(password)
+
+
+def display_asterisks(password):
+    """will display password as asterisks"""
+    for char in range(0, len(password)):
+        print("*", end="")
+
+
+def get_password():
+    """will get valid password"""
+    password = input("Password")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password doesn't meet requirements (minimum {MINIMUM_LENGTH} letters)")
+        password = input("Password: ")
+    return password
+
+
+main()
