14e0e425e3798b2e058e4f2ee8d798f2629a0483
Jack <jackson.doyle@my.jcu.edu.au>
2022-10-02 19:45:45 +1000

    Finished TODO

---
+++ Prac_03/randoms.py
@@ -0,0 +1,18 @@
+"""CP1404 - Practical 3 - Randoms"""
+
+import random
+
+# line 1
+# 20, 6, 18, 8
+# smallest possible number was 5 largest was 20
+
+# line 2
+# 9, 7, 9, 5
+# smallest number 3 and largest 9 could not get a 4
+
+# line 3
+# 2.8113486140116826, 3.922136103449266, 3.108770878428916, 3.519721268724604
+# smallest number 2.5 and largest 5.5
+
+
+print(random.randint(1, 100))
