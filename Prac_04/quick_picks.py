1967468ec7eee921e4de0a897792a5d2f1434073
Jack <jackson.doyle@my.jcu.edu.au>
2022-10-12 19:32:32 +1000

    Prints in order, still need to figure out alignment

--- Prac_04/quick_picks.py
+++ Prac_04/quick_picks.py
@@ -5,15 +5,14 @@ MINIMUM = 1
 MAXIMUM = 45
 NUMBER_OF_NUMBERS = 6
 
-quick_list = []
+numbers = []
 quick_picks = int(input("How many quick picks do you want? "))
-# rows = [pick for pick in range(quick_picks)]
 for picks in range(quick_picks):
     for i in range(NUMBER_OF_NUMBERS):
         number = random.randint(MINIMUM, MAXIMUM)
-        while number in quick_list:
+        while number in numbers:
             number = random.randint(MINIMUM, MAXIMUM)
-        quick_list.append(number)
-        quick_list = sorted(quick_list)
-    print(*quick_list)
-    quick_list.clear()
+        numbers.append(number)
+        numbers = sorted(numbers)
+    print(*numbers)
+    numbers.clear()
