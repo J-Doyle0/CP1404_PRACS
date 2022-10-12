22dbf2514b0423398a6fb21a293f0d43cdd89c22
Jack <jackson.doyle@my.jcu.edu.au>
2022-10-12 18:54:55 +1000

    Prints lists in order, still need to figure out alignment and removing comma

---
+++ Prac_04/quick_picks.py
@@ -0,0 +1,21 @@
+import random
+
+
+MINIMUM = 1
+MAXIMUM = 45
+NUMBER_OF_NUMBERS = 6
+
+quick_list = []
+quick_picks = int(input("How many quick picks do you want? "))
+for picks in range(quick_picks):
+    for i in range(NUMBER_OF_NUMBERS):
+        counter = 0
+        # enumerate(NUMBER_OF_NUMBERS)
+        number = random.randint(MINIMUM, MAXIMUM)
+        while number in quick_list:
+            number = random.randint(MINIMUM, MAXIMUM)
+        quick_list.append(number)
+        quick_list = sorted(quick_list)
+        # print(quick_list, end=' ')
+    print(f"{quick_list}")
+    quick_list.clear()
