2d1bec7fea3ddcfb2035c90ed5cbd15d0c87b182
Jack <jackson.doyle@my.jcu.edu.au>
2022-10-19 20:10:47 +1000

    created file

---
+++ Prac_05/wimbledon.py
@@ -0,0 +1,16 @@
+"""
+wimbledon
+Estimate: 40 minutes
+Actual:   90 minutes
+"""
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    champions_to_wins = {}
+    champion_countries = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+
+
+main()
