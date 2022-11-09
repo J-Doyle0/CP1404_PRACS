c4ed633c339678f8fbb13ef03c95c3172bcb63d7
Jack <jackson.doyle@my.jcu.edu.au>
2022-11-09 19:43:04 +1000

    Finished exercise

--- Prac_07/myguitars.py
+++ Prac_07/myguitars.py
@@ -1,19 +1,43 @@
-
 from Prac_06.guitar import Guitar
+
 FILENAME = 'guitars.csv'
 
 
 def main():
     guitars = get_guitars()
-    guitars.sort()
     for guitar in guitars:
         print(guitar)
+    guitars.sort()
+    add_guitars(guitars)
+    guitars.sort()
+    write_guitars(guitars)
+
+
+def write_guitars(guitars):
+    out_file = open(FILENAME, 'w')
+    for guitar in guitars:
+        out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+    out_file.close()
+
+
+def add_guitars(guitars):
+    """add new guitar to guitars"""
+    print("Add new guitars")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(guitar, "added.")
+        name = input("Name: ")
+    return
 
 
 def get_guitars():
     """read guitar file and saves as list of objects"""
     guitars = []
-    in_file = open('guitars.csv', 'r')
+    in_file = open(FILENAME, 'r')
     for line in in_file:
         guitar = line.strip().split(',')
         name = guitar[0]
@@ -23,9 +47,6 @@ def get_guitars():
         guitars.append(guitar)
     in_file.close()
     return guitars
-
-
-
 
 
 main()
