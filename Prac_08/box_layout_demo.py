29eee8674101b33640f5a71d83c0e4dbaba9e18a
Jack <jackson.doyle@my.jcu.edu.au>
2022-11-16 16:17:31 +1000

    Modified GUI program

--- Prac_08/box_layout_demo.py
+++ Prac_08/box_layout_demo.py
@@ -4,9 +4,31 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """
+        build UI
+        :return:
+        """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def clear_fields(self):
+        """
+        Clear the text input field
+        :return: None
+        """
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
+    def handle_greet(self):
+        """
+        handle greet message
+        :return: None
+        """
+        # print("greet")
+        # print("test")
+        # self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 BoxLayoutDemo().run()
